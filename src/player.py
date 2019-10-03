3a18beaa01ad0e5c9fc42536f8279202ef7ffe80
Carlos Santana <carlossantana987@outlook.com>
2019-10-03 15:56:55 -0400

    mvp day 1 done

--- src/player.py
+++ src/player.py
@@ -5,3 +5,11 @@ class Player:
         self.name = name
         self.current_room = current_room
 
+    def __str__(self):
+        return f'\nYour Current Player and Location:\n  Player =====> {self.name},\n  Location ===> {self.current_room}\n'
+
+    def move(self,direction):
+        if getattr(self.current_room, f"{direction}_to"):
+            self.current_room = getattr(self.current_room, f"{direction}_to")
+        else:
+            print(f"\n !!!!!!!!!There is no path in that direction! TRY AGAIN!!!!!!!!!!")
